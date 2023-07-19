e64fc5b9b76c118fe1aa80ce85cd9b23e2605ef3
ViniciiusCampos <134455972+ViniiciiusSantos@users.noreply.github.com>
2023-07-19 11:35:31 -0300

    menu adicionado

---
+++ script/script.js
@@ -0,0 +1,14 @@
+const iconMenu = document.querySelector('.icone-menu');
+const ul = document.querySelector('ul');
+const fechar = document.querySelector('.fechar-menu');
+
+function abrirMenu() {
+    ul.classList.toggle('active');
+}
+
+function fecharMenu() {
+    ul.classList.add('active')
+}
+
+fechar.addEventListener('click', fecharMenu);
+iconMenu.addEventListener('click', abrirMenu);
